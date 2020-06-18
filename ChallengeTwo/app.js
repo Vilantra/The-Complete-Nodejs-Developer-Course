232d633d023cf54f72aae571e2995ff820a9272b
vilantra <mua.amelie@gmail.com>
2020-06-17 22:26:53 -0400

    ChallengeTwo

---
+++ ChallengeTwo/app.js
@@ -0,0 +1,24 @@
+/*
+ChallengeTwo : Use the chalk library in your project
+
+1. Install version 2.4.1. of chalk
+2. Load chalk into app.js
+3. Use it to print teh string "Success!" to the console in green
+4. Test your work
+
+//Bonus: Use docs to mess around with other styles. Makes text bold and inversed.
+
+*/
+
+const chalk = require('chalk');
+
+const getNotes = require('./notes.js');
+
+const notes = getNotes();
+console.log(chalk.magenta(notes));
+
+console.log(chalk.green.bold('Success!'));
+
+
+// const validator = require('validator')
+// console.log(validator.isEmail('karen.reyes.c@gmail.cl'));
