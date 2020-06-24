4a9f3610ac3151984e00ec1394ce7c8ac0ac6453
vilantra <mua.amelie@gmail.com>
2020-06-24 14:20:42 -0400

    request fetch weather

--- web-server/ChallengeThree/public/js/app.js
+++ web-server/ChallengeThree/public/js/app.js
@@ -1,1 +1,21 @@
-console.log('Client side javascript file is loaded!')
+console.log('Client side javascript file is loaded!');
+
+/*
+Goal: Fetch weather 
+1. Setup a call to fetch to fetch weather for Boston
+2. Get the parse JSON response
+    - If error property, print error
+    - If no error property, print location and forecast
+3. Refresh the browser and test your work
+*/
+
+fetch('http://localhost:3000/weather?address=temuco').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error);
+        } else {
+            console.log(data.location);
+            console.log(data.forecast);
+        }
+    })
+})
